fb60b22c4a91f8dfdaaf870f5da3f05b1644cedc
ShivamA96 <1100avi@gmail.com>
2024-04-14 21:56:11 +0530

    added backend functionalites

--- torrent_service/app/api/endpoints.py
+++ torrent_service/app/api/endpoints.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends
 from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.exceptions import HTTPException
 router = APIRouter()
+
 from prisma import Client
 
 async def get_db():
@@ -16,7 +18,7 @@ def read_root():
     return {"message": "Welcome to the Torrent Service"}
 
 @router.post("/torrents")
-async def create_torrent(db: Client = Depends(get_db), File: UploadFile = File(...)):
+async def create_torrent(db: Client = Depends(get_db), File: UploadFile = File(...), body: dict = Form(...)):
 
     if not File.filename:
         raise HTTPException(status_code=400, detail="File is required")
@@ -28,28 +30,34 @@ async def create_torrent(db: Client = Depends(get_db), File: UploadFile = File(.
     torrent = await db.torrent.create({
         "title": File.filename,
         "path": f"static/{File.filename}",
-        "description": "Dummy description",
+        "description": body["description"],
         "uploaderId": "dummyUploaderId",
-        "size": 123.45,
-        "category": "Dummy category",
-        "type": "Dummy type",
+        "size": body["size"],
+        "category": body["category"],
+        "type": body["type"],
         "downloads": 0,
         "health": 100.0,  
     })
 
     return {"message": "Torrent created successfully", "torrent": torrent}
     
-# @router.get("/torrents")
-# def get_torrents():
-#     # TODO: Implement logic to retrieve all torrents
-#     return {"message": "Get all torrents"}
+@router.get("/torrents")
+def get_torrents(db: Client = Depends(get_db)):
+    # TODO: Implement logic to retrieve all torrents
+    torrents = db.torrent.findMany()
+
+    if not torrents:
+        raise HTTPException(status_code=404, detail="No torrents found")
+    return {"Torrents": torrents}
 
 
-# @router.get("/torrents/{torrent_id}")
-# def get_torrent(torrent_id: int):
-#     # TODO: Implement logic to retrieve a specific torrent by ID
-#     return {"message": f"Get torrent with ID {torrent_id}"}
 
+@router.get("/torrents/{torrent_id}")
+async def get_torrent(torrent_id: int, db: Client = Depends(get_db)):
+    torrent = await db.torrent.find_unique(where={"id": torrent_id})
+    if not torrent:
+        raise HTTPException(status_code=404, detail="Torrent not found")
+    return {"Torrent": torrent}
 
 # @router.post("/torrents")
 # def create_torrent():
